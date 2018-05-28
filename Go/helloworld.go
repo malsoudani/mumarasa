6dd9575f241f867bcd477591d1776dc132103302
Mohamed Alsoudani <mr.hassuny@gmail.com>
2018-05-28 12:49:36 -0600

    pointer manipulation and references

---
+++ Go/helloworld.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message = "Hello Go World!"
+	var greeting *string = &message
+
+	fmt.Println(message, greeting);
+	// output is: Hello Go World! 0xc82000a2f0
+
+	fmt.Println(message, *greeting);
+	//output is: Hello Go World! Hello Go World!
+}
