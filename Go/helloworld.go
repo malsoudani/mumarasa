6fd700e46ff59ec0cacc242dea0b36e7c96b5d2f
Mohamed Alsoudani <mr.hassuny@gmail.com>
2018-05-28 14:30:16 -0600

    function returns in golang

--- Go/helloworld.go
+++ Go/helloworld.go
@@ -2,18 +2,36 @@ package main
 
 import "fmt"
 
-func main() {
-	var message = "Hello Go World!"
-	var greeting *string = &message
+type Salutation struct {
+	name string
+	greeting string
+}
 
-	fmt.Println(message, greeting);
+func CreateMessage(a, b string) string {
+	return b + " " + a
+}
+
+func Greet(salutation Salutation) { //function definition example
+	fmt.Println(CreateMessage(salutation.name, salutation.greeting))
+}
+
+func main() {
+	// var message = "Hello Go World!"
+	// var greeting *string = &message
+
+	// fmt.Println(message, greeting);
 	// output is: Hello Go World! 0xc82000a2f0
 
-	fmt.Println(message, *greeting);
+	// fmt.Println(message, *greeting);
 	//output is: Hello Go World! Hello Go World!
 	
-	*greeting = "hi"
-	fmt.Println(message, *greeting);
+	// *greeting = "hi"
+	// fmt.Println(message, *greeting);
 	//output is: hi hi
+
+	var s = Salutation{"Bob", "wassap"} // example of using user defined type
+
+	Greet(s)
+
 
 }
