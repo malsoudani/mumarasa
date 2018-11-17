81f0f17d7551a05b5484d6cb8c52369db4ba891a
malsoudani <mr.hassuny@gmail.com>
2018-11-16 23:08:21 -0700

    played a little with meteor

--- javascript/meteor/TodoList/client/main.js
+++ javascript/meteor/TodoList/client/main.js
@@ -1,22 +1,10 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
+import { Todos } from '../libs/Todos.js';
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.main.helpers({
+  todos() {
+    return Todos.find({});
   },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+})
+ 
