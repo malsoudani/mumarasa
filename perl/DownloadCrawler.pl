d68eaf72b401134ada6bf83c0d20df967c07e6af
Mohamed Alsoudani <mr.hassuny@gmail.com>
2019-01-03 18:28:57 -0700

    adding a web crawler

---
+++ perl/DownloadCrawler.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+use LWP::Simple;
+
+sub main {
+    print "Downloading ... \n";
+    # print get("http://www.google.com");
+
+    my $httpCode = getstore("https://croatia.hr/sites/default/files/styles/image_full_width/public/2017-08/01_01_slide_nature.jpg", "nature.jpg");
+    if ($httpCode == 200) {
+        print "Success\n";
+    } else {
+        print $httpCode;
+    }
+    print "Finished";
+
+}
+
+main();
