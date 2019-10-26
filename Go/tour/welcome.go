d884983754bfd0f8ea63f20e7a89de47987054dd
Mohamed Alsoudani <mr.hassuny@gmail.com>
2019-10-26 16:52:25 +0000

    using the time package and printing to standard output (Golang tour)

--- Go/tour/welcome.go
+++ Go/tour/welcome.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	// this is how we print to the standard output in go
 	fmt.Println("Hello World")
+
+	// example usage of the time package
+	fmt.Println("the time is: ", time.Now())
 }
