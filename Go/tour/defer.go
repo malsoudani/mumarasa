5eece14e3590163bed88473e20dcfc9e40b45de5
Mohamed Alsoudani <mr.hassuny@gmail.com>
2019-10-28 06:14:39 +0000

    deferring stuff

--- Go/tour/defer.go
+++ Go/tour/defer.go
@@ -10,3 +10,22 @@ func main() {
 	defer fmt.Println("world")
 	fmt.Println("hello")
 }
+
+
+// when defer satements get called multiple times in a single function they actually get pushed into a stack and get excuted in a LIFO (Last In First Out) order
+
+// comment out the top example and uncomment this one
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("counting")
+
+// 	for i := 0; i < 10; i++ {
+// 		defer fmt.Println(i)
+// 	}
+
+// 	fmt.Println("done")
+// }
