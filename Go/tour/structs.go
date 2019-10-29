002b3622f00b3ff6e2d2caa8f1d3f5399c604146
Mohamed Alsoudani <mr.hassuny@gmail.com>
2019-10-29 21:21:51 +0000

    go structs .. go tour

---
+++ Go/tour/structs.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Rectangle struct {
+	x int
+	y int
+}
+
+func main() {
+	rect := Rectangle{12, 44}
+	fmt.Println(rect.x, rect.y)
+
+
+	another := Rectangle{
+		y: 1233,
+		x: 2e3,
+	}
+
+	fmt.Println(another.x)
+}
