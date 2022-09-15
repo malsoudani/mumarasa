c867981e7c96f290b1e649bec6ffb6a71cf34530
Mohamed Alsoudani <mohamedalsoudani@Mohameds-MacBook-Pro-2.local>
2022-09-15 10:38:49 -0600

    typescript + react todo initial

---
+++ typescript/todo-react-app/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import { Todo } from './model';
+
+let name: string;
+let age: number;
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTodo("");
+
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
+    }
+  }
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos} />
+
+      {/* {todos.map((todo) => (<li>{todo.todo}</li>))} */}
+    </div>
+  );
+};
+
+export default App;
---
+++ typescript/todo-react-app/src/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Todo } from '../model'
+
+interface Props {
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({todos, setTodos}) => {
+  return (
+    <div className='todos'>
+        {todos.map((t) => <li>{t.todo}</li>)}
+    </div>
+  )
+}
+
+export default TodoList
